7af9a8c2adc8efdfda21de461d5f4e562d671850
Vincent <r45635@users.noreply.github.com>
2026-06-07 08:00:45 -0400

    Fix bcrypt hash extraction in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -118,7 +118,7 @@ ensure_env_files() {
 generate_password_hash() {
   local admin_password="$1"
 
-  docker run --rm ghcr.io/wg-easy/wg-easy:14 wgpw "$admin_password" | grep -oE '\$2[aby]\$[./A-Za-z0-9]{56}' | head -n 1
+  docker run --rm ghcr.io/wg-easy/wg-easy:14 wgpw "$admin_password" | grep -oE '\$2[aby]\$[./A-Za-z0-9]{53}' | head -n 1
 }
 
 main() {
